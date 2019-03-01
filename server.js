410ee597e0166b1c7c6775e7a89973673155ce85
Glitch (woolen-rainbow) <none>
2019-03-01 12:34:59 +0000

    🛳📺 Checkpoint ./server.js:3066526/1549

--- server.js
+++ server.js
@@ -19,16 +19,24 @@ mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
 
 const Schema = mongoose.Schema;
 
+const logSchema = new Schema({
+  count: Number,
+  log: [{description: String, duration: Number, date: Date}]
+});
+
+
+
+
 const userSchema = new Schema({
-    username:  String,
-    count: Number,
-    log: [{description: String, duration: Number, date: Date}]
-  });
-// const logSchema = new Schema({
-//   count: Number,
-//   log: [{description: String, duration: Number, date: Date}]
-// });
+  username:  String,
+  count: Number,
+  // log: [{description: String, duration: Number, date: Date}]
+  child: logSchema
+});
+
 const User = mongoose.model('User', userSchema);
+
+const Log = mongoose.model('Log', logSchema);
 
 
 app.use(cors())
@@ -46,12 +54,11 @@ app.get('/', (req, res) => {
 app.post('/api/exercise/new-user',(req, res, next) => {
   const newUser = new User({
       username: req.body.username
-    })
+    });
   newUser.save((err) => {
     if(err) console.log(err);
-  })
+  });
   res.send({"username":newUser.username,"id":newUser._id});
-  // next();
 });
 
 // Get all users
@@ -71,23 +78,32 @@ app.post('/api/exercise/add', (req, res, next) => {
   const des = req.body.description;
   const dur = req.body.duration;
   let date = req.body.date;
-  // validate Date input
-  if (date === '') {
-    let newDate = Date.now();
-    date =  moment(newDate).format('YYYY-MM-DD');
-  } else if (!moment(req.body.date, 'YYYY-MM-DD',true).isValid()) {
-    res.json(`${date} is an invalid date. Please enter a valid date in the format YYYY-MM-DD`);
-    return;
-  }
-  User.findById(id, (err, user) => {
-    user.log = user.log.concat([{description: des, duration: dur, date: date}]);
-    let logCount = user.log.length;
-    user.count = logCount;
-    user.save((err) => {
-      if(err) console.log(`findById error: ${err}`);
-    });
-    res.send({username: user.username, description: des, duration: dur, id: id, date: date});
+  
+  let logArray = []
+  
+  const newLog = new Log({
+    log: logArray.concat([{description: des, duration: dur, date: date}])
   });
+  
+  console.log(newLog);
+  
+  // validate Date input
+  // if (date === '') {
+  //   let newDate = Date.now();
+  //   date =  moment(newDate).format('YYYY-MM-DD');
+  // } else if (!moment(req.body.date, 'YYYY-MM-DD',true).isValid()) {
+  //   res.json(`${date} is an invalid date. Please enter a valid date in the format YYYY-MM-DD`);
+  //   return;
+  // }
+  // User.findById(id, (err, user) => {
+  //   user.log = user.log.concat([{description: des, duration: dur, date: date}]);
+  //   let logCount = user.log.length;
+  //   user.count = logCount;
+  //   user.save((err) => {
+  //     if(err) console.log(`findById error: ${err}`);
+  //   });
+  //   res.send({username: user.username, description: des, duration: dur, id: id, date: date});
+  // });
 });
 
 // Get user logs, optionally sort
@@ -101,9 +117,9 @@ app.get('/api/exercise/log?:userId', (req, res, next) => {
   
   User.findById(userId, (err, user) => {
     if (err) console.log(`error in /api/exercise/log: ${err}`);
-    let filtered = user.log.filter(logObj => {
-      logObj
-    })
+    // let filtered = user.log.filter(logObj => {
+    //   logObj
+    // })
     res.send(user);
   });
   // next();
