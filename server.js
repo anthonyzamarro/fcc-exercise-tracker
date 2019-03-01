5e0d000b2a4a5e6bd1b37ec1ec4bd7c416d1e762
Glitch (woolen-rainbow) <none>
2019-03-01 12:56:00 +0000

    📫🍫 Checkpoint ./server.js:3066526/2234

--- server.js
+++ server.js
@@ -19,24 +19,16 @@ mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
 
 const Schema = mongoose.Schema;
 
-const logSchema = new Schema({
-  count: Number,
-  log: [{description: String, duration: Number, date: Date}]
-});
-
-
-
-
 const userSchema = new Schema({
-  username:  String,
-  count: Number,
-  // log: [{description: String, duration: Number, date: Date}]
-  child: [logSchema]
-});
-
+    username:  String,
+    count: Number,
+    log: [{description: String, duration: Number, date: Date}]
+  });
+// const logSchema = new Schema({
+//   count: Number,
+//   log: [{description: String, duration: Number, date: Date}]
+// });
 const User = mongoose.model('User', userSchema);
-
-const Log = mongoose.model('Log', logSchema);
 
 
 app.use(cors())
@@ -54,11 +46,12 @@ app.get('/', (req, res) => {
 app.post('/api/exercise/new-user',(req, res, next) => {
   const newUser = new User({
       username: req.body.username
-    });
+    })
   newUser.save((err) => {
     if(err) console.log(err);
-  });
+  })
   res.send({"username":newUser.username,"id":newUser._id});
+  // next();
 });
 
 // Get all users
@@ -78,14 +71,6 @@ app.post('/api/exercise/add', (req, res, next) => {
   const des = req.body.description;
   const dur = req.body.duration;
   let date = req.body.date;
-  
-  let logArray = []
-  
-  const newLog = new Log({
-    log: logArray.concat([{description: des, duration: dur, date: date}])
-  });
-
-  
   // validate Date input
   if (date === '') {
     let newDate = Date.now();
@@ -95,10 +80,9 @@ app.post('/api/exercise/add', (req, res, next) => {
     return;
   }
   User.findById(id, (err, user) => {
-    user.child = user.child.concat([Log]);
-    // let logCount = user.log.length;
-    // user.count = logCount;
-    console.log(user);
+    user.log = user.log.concat([{description: des, duration: dur, date: date}]);
+    let logCount = user.log.length;
+    user.count = logCount;
     user.save((err) => {
       if(err) console.log(`findById error: ${err}`);
     });
@@ -117,13 +101,9 @@ app.get('/api/exercise/log?:userId', (req, res, next) => {
   
   User.findById(userId, (err, user) => {
     if (err) console.log(`error in /api/exercise/log: ${err}`);
-    // let filtered = user.log.filter(logObj => {
-    //   logObj
-    // })
-    user.child.forEach(l => {
-      console.log(l)
-    });
-    
+    let filtered = user.log.filter(logObj => {
+      logObj
+    })
     res.send(user);
   });
   // next();
