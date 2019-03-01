75882d7861bfd519450b0dc1480667aa6953254c
Glitch (woolen-rainbow) <none>
2019-03-01 03:04:29 +0000

    🍽👄 Checkpoint ./server.js:3066526/614

--- server.js
+++ server.js
@@ -19,16 +19,20 @@ mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
 
 const Schema = mongoose.Schema;
 
+const logSchema = new Schema({
+  count: Number,
+  log: [{description: String, duration: Number, date: Date}]
+});
+
 const userSchema = new Schema({
     username:  String,
     count: Number,
-    log: [{description: String, duration: Number, date: Date}]
+    // log: [{description: String, duration: Number, date: Date}]
+    child: logSchema
   });
-// const logSchema = new Schema({
-//   count: Number,
-//   log: [{description: String, duration: Number, date: Date}]
-// });
+
 const User = mongoose.model('User', userSchema);
+const Log = mongoose.model('Log', logSchema);
 
 
 app.use(cors())
@@ -80,12 +84,13 @@ app.post('/api/exercise/add', (req, res, next) => {
     return;
   }
   User.findById(id, (err, user) => {
-    user.log = user.log.concat([{description: des, duration: dur, date: date}]);
-    let logCount = user.log.length;
-    user.count = logCount;
-    user.save((err) => {
-      if(err) console.log(`findById error: ${err}`);
-    });
+    console.log(user)
+    // user.log = user.log.concat([{description: des, duration: dur, date: date}]);
+    // let logCount = user.log.length;
+    // user.count = logCount;
+    // user.save((err) => {
+    //   if(err) console.log(`findById error: ${err}`);
+    // });
     res.send({username: user.username, description: des, duration: dur, id: id, date: date});
   });
 });
@@ -101,7 +106,7 @@ app.get('/api/exercise/log?:userId', (req, res, next) => {
   
   User.findById(userId, (err, user) => {
     if (err) console.log(`error in /api/exercise/log: ${err}`);
-    // const requestedUser = user.log
+    // const requestedUser = user.log.find('5c789b42854b6a0c19698efd');
     console.log(user);
     res.send(user);
   });
